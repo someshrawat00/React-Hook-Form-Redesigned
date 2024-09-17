f31d9af25c34ecf13ac68285d3b7d791f1540787
someshrawat00 <someshrawat00@gmail.com>
2024-09-17 18:38:13 +0530

    added icons

--- src/Form.jsx
+++ src/Form.jsx
@@ -18,17 +18,20 @@ function Form() {
         </div>
         <form className="" onSubmit={handleSubmit(signUp)}>
           <div className="flex w-full justify-between mb-8 items-center">
-            <h1 className='text-2xl font-medium'>Log In</h1>
-            <div className="px-4 py-2 rounded-full border border-gray-500 text-xs cursor-pointer">Facebook</div>
+            <h1 className='text-3xl font-normal'>Log In</h1>
+            <div className="px-3 py-2 rounded-full border items-center flex gap-1 border-gray-500 text-xs cursor-pointer">
+              <img src="/facebook.svg" alt="" className='w-5' />
+              Facebook
+              </div>
           </div>
           <div className="mb-6">
-            <Input placeholder = "e-mail address" type= "email" {...register("email")}/>
+            <Input placeholder = "e-mail address" img = "/at_sign.svg" type= "email" {...register("email")}/>
           </div>
           <div className="mb-10">
-            <Input placeholder = "password" type = "password" btntext = "I&nbsp;forgot" btn = "true" {...register("password")}/>
+            <Input placeholder = "password" img = "/key.svg" type = "password" btntext = "I&nbsp;forgot" btn = "true" {...register("password")}/>
           </div>
           <div className="flex justify-between mb-10">
-            <p className='text-xs text-gray-800 w-9/12'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae error porro delectus qui</p>
+            <p className='text-xs text-gray-800 w-9/12'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi obcaecati laudantium illo culpa a voluptates.</p>
             <button type='submit' className="p-3 bg-black text-white rounded-full cursor-pointer">Go</button>
           </div>
         </form>
--- src/Input.jsx
+++ src/Input.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Input({
   type = "text",
   btn = "",
+  img = "",
   btntext = "",
   placeholder = "",
   ...props
@@ -12,7 +13,9 @@ function Input({
   return (
     <div className='w-full flex rounded-full box-border font-medium h-fit justify-between items-center bg-[rgba(255,255,255,.3)] p-2 border-grey-500'>
       <div className="flex">
-        <div className="w-[40px] h-[40px] rounded-full bg-white bg-[rgba(255,255,255,.5)]"></div>
+        <div className="w-[40px] h-[40px] rounded-full flex items-center justify-center p-3 bg-white bg-[rgba(255,255,255,.5)]">
+          <img src={img} alt="" className='w-full'/>
+        </div>
         <input type={type} placeholder={placeholder} className='outline-none focus:bg-none w-10/12 border-none bg-transparent px-4 placeholder:text-gray-700' ref={ref} {...props} />
       </div>
       {btn && <div className="rounded-full flex cursor-pointer w-max justify-center items-center px-4 h-[35px] bg-white text-xs">{btntext}</div>}
